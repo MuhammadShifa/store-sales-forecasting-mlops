f2301ce945a5f00331af94ab78ea4a8d9ca1be5e
muhammadshifa <muhammadshifa102@gmail.com>
2025-07-31 21:04:20 +0500

    updated the script for testing kinesis with localstack

--- 6-best_practices/code/integration-test/run.sh
+++ 6-best_practices/code/integration-test/run.sh
@@ -7,22 +7,43 @@ LOCAL_TAG=`date +"%Y-%m-%d-%H-%M"`
 LOCAL_IMAGE_NAME="stream-model-sales-predictions:${LOCAL_TAG}"
 
 export LOCAL_IMAGE_NAME="stream-model-sales-predictions:${LOCAL_TAG}"
+export PREDICTIONS_STREAM_NAME="sales_predictions"
 
 
 docker build -t ${LOCAL_IMAGE_NAME} ..
 
+
 docker-compose up -d
 
+
 sleep 5
+
+
+aws --endpoint-url=http://localhost:4566 \
+    kinesis create-stream \
+    --stream-name ${PREDICTIONS_STREAM_NAME} \
+    --shard-count 1
+
 
 pipenv run python test_docker.py 
 
 ERROR_CODE=$?
 
-if [ ${ERROR_CODE} != 0]; then
+
+if [ ${ERROR_CODE} != 0 ]; then
     docker-compose logs
+    docker-compose down
+    exit ${ERROR_CODE}
+fi
+
+pipenv run python test_kinesis.py
+
+ERROR_CODE=$?
+
+if [ ${ERROR_CODE} != 0 ]; then
+    docker-compose logs
+    docker-compose down
+    exit ${ERROR_CODE}
 fi
 
 docker-compose down
-
-exit ${ERROR_CODE}
---
+++ 6-best_practices/code/integration-test/test_kinesis.py
@@ -0,0 +1,54 @@
+# pylint: disable=duplicate-code
+
+import os
+import json
+from pprint import pprint
+
+import boto3
+from deepdiff import DeepDiff
+
+kinesis_endpoint = os.getenv('KINESIS_ENDPOINT_URL', "http://localhost:4566")
+kinesis_client = boto3.client('kinesis', endpoint_url=kinesis_endpoint)
+
+stream_name = os.getenv('PREDICTIONS_STREAM_NAME', 'sales_predictions')
+shard_id = 'shardId-000000000000'
+
+
+shard_iterator_response = kinesis_client.get_shard_iterator(
+    StreamName=stream_name,
+    ShardId=shard_id,
+    ShardIteratorType='TRIM_HORIZON',
+)
+
+shard_iterator_id = shard_iterator_response['ShardIterator']
+
+
+records_response = kinesis_client.get_records(
+    ShardIterator=shard_iterator_id,
+    Limit=1,
+)
+
+
+records = records_response['Records']
+
+assert len(records) == 1
+
+
+actual_record = json.loads(records[0]['Data'])
+pprint("actual_record")
+pprint(actual_record)
+
+expected_record = {
+    'model': 'sales_prediction_model',
+    'version': 'Test123',
+    'prediction': {
+        'sales_prediction': 222.69079987424266,
+        'sales_id': 512
+  }
+}
+diff = DeepDiff(actual_record, expected_record, significant_digits=1)
+print(f'diff={diff}')
+
+assert 'values_changed' not in diff
+assert 'type_changes' not in diff
+
